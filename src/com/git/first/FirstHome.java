522d88af3b0c39a0147f0953de4e544aabe5d034
shs <sudhakar@codem.com>
2019-09-11 12:33:54 +0530

    Branch 2

--- src/com/git/first/FirstHome.java
+++ src/com/git/first/FirstHome.java
@@ -6,6 +6,11 @@ public class FirstHome {
 		// TODO Auto-generated method stub
 		System.out.println("Hello EGit");
 		System.out.println("Hello to versioning ....");
+		System.out.println("Creating branch1");
+	}
+	
+	public static void main2(String args) {
+		System.out.println("I am the main2 method");
 	}
 
 }
